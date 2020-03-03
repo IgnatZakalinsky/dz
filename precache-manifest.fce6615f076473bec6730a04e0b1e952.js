d505b5c889c7a7ae91f79364064eacac2c8b83cb
Ignat <ai73a@yandex.ru>
2020-03-03 20:49:20 +0300

    Updates

---
+++ precache-manifest.fce6615f076473bec6730a04e0b1e952.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7ef425d42c208b64c14a8d58c6f8f6c",
+    "url": "/dz/index.html"
+  },
+  {
+    "revision": "8c07b6fcf0a745461a80",
+    "url": "/dz/static/css/main.eab24d3c.chunk.css"
+  },
+  {
+    "revision": "2dc83dbca2bee6bbccc4",
+    "url": "/dz/static/js/2.191b783d.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/dz/static/js/2.191b783d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c07b6fcf0a745461a80",
+    "url": "/dz/static/js/main.2c60b6dc.chunk.js"
+  },
+  {
+    "revision": "1f80dac5b44828ca5443",
+    "url": "/dz/static/js/runtime-main.335a7a9e.js"
+  }
+]);
